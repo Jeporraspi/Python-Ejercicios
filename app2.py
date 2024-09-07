f1db08dfe8b584cd7e4c45c20fd2ad5f35f614a1
jeporraspi <jeporraspi@cesde.net>
2024-09-06 20:18:50 -0500

    c3jp

--- app2.py
+++ app2.py
@@ -28,3 +28,43 @@ for _ in range(cantidad_numeros):
 
 nueva_lista = eliminar_duplicados(numeros)
 print("Lista sin duplicados:", nueva_lista)"""
+
+"""
+import random
+
+def adivina_numero():
+    
+    numero = random.randint(1, 100)
+    intentos = 7
+
+    print("Adivinar un número con intentos limitados:")
+    print("Tienes 7 intentos para adivinar un número entre 1 y 100.")
+
+    while intentos > 0:
+        intento = int(input("Ingresa número: "))
+
+        if intento == numero:
+            print(f" Adivinaste el número en {7 - intentos + 1} intentos.")
+            break
+        elif intento < numero:
+            print("El número es mayor.")
+        else:
+            print("El número es menor.")
+
+        intentos -= 1
+        print(f"Te quedan {intentos} intentos.")
+
+    if intentos == 0:
+        print(f"Perdiste. El número secreto era {numero_secreto}.")
+
+if __name__ == "__main__":
+    adivina_numero()  """
+"""
+numeros_pares = []
+contador = 2
+
+while contador <= 100:
+  numeros_pares.append(contador)
+  contador += 2
+
+print(numeros_pares)"""
--- app3.py
+++ app3.py
@@ -1,1 +1,21 @@
-print{"hola"}
+"""
+def promediodenotas():
+    notas = {}
+    cantidad_estudiantes = int(input("cantidad de estudiantes: "))
+    for _ in range(cantidad_estudiantes):
+        nombre = input("Ingrese el nombre del alumno: ")
+        nota = float(input(f"Ingrese la nota de {nombre}: "))
+        notas[nombre] = nota
+    suma_notas = 0
+    cantidad_notas = 0
+
+    for alumno, nota in notas.items():
+        suma_notas += nota
+        cantidad_notas += 1
+
+    promedio = suma_notas / cantidad_notas
+    return promedio
+
+promedio = promediodenotas()
+print("El promedio:", promedio)
+"""
