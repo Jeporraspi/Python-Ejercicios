edcdc206ddacef92eafe4de5e1d673aca2228073
Wilmer Agudelo <wilmer.com.co@gmail.com>
2024-09-07 11:49:17 -0500

    ejercicios Aleja

--- app3.py
+++ app3.py
@@ -1,1 +1,18 @@
-print{"hola"}
+#Ejercicios con Diccionarios
+#1. Crear un diccionario de alumnos y sus notas.
+alumnos = {
+        'Ana':4.5,
+        'Marcela': 4.0,
+        'Sergio':3.5,
+        'Aleja':4.8,
+        'Leidy': 5.0
+        }
+
+print(alumnos)
+
+#5. Buscar una palabra en un diccionario
+#Buscar a Marcela
+if 'Marcela' in alumnos:
+    print('Marcela está en el diccionario')
+else:
+    ('Marcela no está en el diccionario')
